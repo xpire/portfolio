8215dc7034e1a7c70adc21349bd17278eaa9ff57
Justin Or <justin.or@student.unsw.edu.au>
2020-06-06 22:22:26 +1000

    fix(*): fixed some issues listed in todo

--- src/components/projects/card/ProjectDialog.tsx
+++ src/components/projects/card/ProjectDialog.tsx
@@ -5,7 +5,11 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
+import styled from "styled-components";
 import { motion } from "framer-motion";
 
 type Props = {
@@ -15,7 +19,15 @@ type Props = {
   content: React.ReactNode;
   src: string;
   external: string;
+  category: string;
 };
+
+const StyledImg = styled(motion.img)`
+  display: flex;
+  align-items: center;
+  height: auto;
+`;
+
 const ProjectDialog = ({
   open,
   setOpen,
@@ -23,6 +35,7 @@ const ProjectDialog = ({
   content,
   src,
   external,
+  category,
 }: Props) => {
   return (
     <Dialog
@@ -32,22 +45,10 @@ const ProjectDialog = ({
       maxWidth="md"
       fullWidth={true}
     >
-      <DialogTitle>{title}</DialogTitle>
-      <DialogContent dividers>
-        {content}
-        <motion.img
-          src={src}
-          initial={{ scale: 0.9, opacity: 0 }}
-          animate={{ scale: 1, opacity: 1 }}
-          style={{
-            // width: "80vw",
-            maxWidth: "100%",
-            display: "flex",
-            alignItems: "center",
-            height: "auto",
-          }}
-        />
-      </DialogContent>
+      <DialogTitle>
+        <Typography variant="h5">{title}</Typography>
+        <Typography variant="body2">{category}</Typography>
+      </DialogTitle>
       <DialogActions>
         <Button
           // autoFocus
@@ -57,6 +58,17 @@ const ProjectDialog = ({
           See Source
         </Button>
       </DialogActions>
+      <DialogContent dividers>
+        {content}
+        <Paper elevation={5}>
+          <StyledImg
+            src={src}
+            initial={{ scale: 0.9, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            style={{ maxWidth: "100%" }}
+          />
+        </Paper>
+      </DialogContent>
     </Dialog>
   );
 };
