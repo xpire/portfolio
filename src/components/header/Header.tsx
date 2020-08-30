c14994625c699b5d866d07aa82a12d26e330a575
Justin Or <justin.or@student.unsw.edu.au>
2020-08-30 15:41:20 +1000

    chore(m*): move to v1 subdomain

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,20 +1,8 @@
-import React, { useState, useCallback, useEffect } from "react";
-import styled from "styled-components";
+import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
-import { useTransition, animated } from "react-spring";
-import { motion } from "framer-motion";
 import AppBar from "@material-ui/core/AppBar";
-import BottomNavigation from "@material-ui/core/BottomNavigation";
-import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
-import Button from "@material-ui/core/Button";
 import Link from "@material-ui/core/Link";
-
-import FolderIcon from "@material-ui/icons/Folder";
-import CallIcon from "@material-ui/icons/Call";
-import InfoIcon from "@material-ui/icons/Info";
-import HomeIcon from "@material-ui/icons/Home";
-import logo from "../../website.logo.svg";
 
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import Slide from "@material-ui/core/Slide";
@@ -22,9 +10,11 @@ import Slide from "@material-ui/core/Slide";
 function HideOnScroll(props: { children: React.ReactElement }) {
   const { children } = props;
   const trigger = useScrollTrigger({ target: window });
+  const userAgentString = navigator.userAgent;
+  const chromeAgent = userAgentString.indexOf("Chrome") > -1; // to detect if chrome, backdrop-filter does not work on firefox :( 8/8/20
 
   return (
-    <Slide appear={false} direction="down" in={!trigger}>
+    <Slide appear={false} direction="down" in={!trigger || chromeAgent}>
       {children}
     </Slide>
   );
@@ -43,46 +33,16 @@ const Title = () => {
     }
   };
 
-  // window.addEventListener("load", () => {
-  //   const headings = document.querySelectorAll("h1 a[name]");
-
-  //   document.addEventListener("scroll", (e) => {
-  //     headings.forEach((ha) => {
-  //       const rect = ha.getBoundingClientRect();
-  //       if (rect.top > 0 && rect.top < 150) {
-  //         const location = window.location.toString().split("#")[0];
-  //         history.replaceState(null, "", location + "#" + ha.name);
-  //       }
-  //     });
-  //   });
-  // });
-
-  // const listenScrollEvent = (e:any) => {
-  //   window.scrollY % window.innerHeight
-  //   if (window.scrollY > window.innerHeight) {
-  //     setValue("")
-  // }
-
-  // componentDidMount() {
-  //   window.addEventListener('scroll', listenScrollEvent)
-  // }
-
+  const userAgentString = navigator.userAgent;
+  const chromeAgent = userAgentString.indexOf("Chrome") > -1;
   return (
     <HideOnScroll>
       <AppBar
         style={{
-          // marginLeft: 2,
-          background: "black",
+          background: chromeAgent ? "rgba(0, 0, 0, 0.5)" : "rgba(0, 0, 0, 0.9)",
+          backdropFilter: chromeAgent ? "blur(10px)" : "none", // to detect if chrome, backdrop-filter does not work on firefox :( 8/8/20
         }}
       >
-        {/* <img
-          src={logo}
-          style={{
-            filter: "invert(100%)",
-            height: "1em",
-            padding: "6px",
-          }}
-        /> */}
         <Grid container spacing={1} justify="flex-start" alignItems="stretch">
           <Grid item xs={3} md={1}>
             <Link style={{ color: "white" }} onClick={handleChange}>
@@ -105,40 +65,6 @@ const Title = () => {
             </Link>
           </Grid>
         </Grid>
-
-        {/* <BottomNavigation
-          value={value}
-          onChange={handleChange}
-          style={{
-            marginRight: 2,
-            background: "black",
-          }}
-        >
-          <BottomNavigationAction
-            label="Home"
-            value="home"
-            icon={<HomeIcon />}
-            style={{ color: "white" }}
-          />
-          <BottomNavigationAction
-            label="About"
-            value="about"
-            style={{ color: "white" }}
-            icon={<InfoIcon />}
-          />
-          <BottomNavigationAction
-            label="Projects"
-            value="projects"
-            style={{ color: "white" }}
-            icon={<FolderIcon />}
-          />
-          <BottomNavigationAction
-            label="Contact"
-            value="contact"
-            style={{ color: "white" }}
-            icon={<CallIcon />}
-          />
-        </BottomNavigation> */}
       </AppBar>
     </HideOnScroll>
   );
